ac900289779f0c7304a3d27bb367c99365391d4e
sergioruizj <sergio.ruizj@edu.uah.es>
2025-04-25 11:46:22 +0200

    Tests 5.3

--- Tema5_Ejercicio3.py
+++ Tema5_Ejercicio3.py
@@ -1,9 +1,9 @@
-def encontrar_combinaciones(cadena, N):
+def encontrar_combinaciones(cadena: str, n: int):
     resultados = set()
 
     def backtrack(indice, camino):
         # Si hemos formado un número de N cifras, lo añadimos al resultado
-        if len(camino) == N:
+        if len(camino) == n:
             resultados.add(''.join(camino))
             return
         # Si ya no quedan suficientes dígitos, paramos
@@ -42,3 +42,25 @@ def test_encontrar_combinaciones():
     assert "12" == combinaciones[0]
     assert "13" == combinaciones[1]
     assert "23" == combinaciones[2]
+
+def test_encontrar_combinaciones0():
+    cadena = ""
+
+    for i in range(1,50):
+        combinaciones = encontrar_combinaciones(cadena, i)
+        assert len(combinaciones) == 0
+
+def test_timer_encontrar_combinaciones():
+    import Tests_timer
+    
+    @Tests_timer.timer
+    def _timer_encontrar_combinaciones(cadena: str, n: int):
+        return encontrar_combinaciones(cadena, n)
+    
+    cadenaA = "1234765375"
+    Tests_timer.warmup()
+    resultado = _timer_encontrar_combinaciones(cadenaA, 3)
+
+    assert len(resultado[0]) == 89
+    print(f'\n\nSe ha empleado un total de {resultado[1]} ms en la ejecución del programa.\n')
+
